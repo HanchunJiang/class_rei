b9b9c92d83ce60e533e8fe60d074f9a1013aaa5c
HanchunJiang <hcjiang@g.ecc.u-tokyo.ac.jp>
2023-11-28 15:36:15 +0900

    finish week2

--- week2_noise/error.py
+++ week2_noise/error.py
@@ -1,19 +1,35 @@
 import numpy as np
-import matpoltlib.pyplot as plt
+import matplotlib.pyplot as plt
 f_sky=1
 delta_l=1
 NNs=np.load("spectrum.npy")
 
-files=['/Users/hanchunjiang/class_public-3.2.0/output/reio_camb02_cl.dat']
+#files=['/Users/hanchunjiang/class_public-3.2.0/output/reio_camb02_cl.dat']
+files=['/Users/hanchunjiang/class_public-3.2.0/output/reio_camb03_cl_lensed.dat']
 data=[]
 for data_file in files:
     data.append(np.loadtxt(data_file))
 
 curve=data[0]
-TTs=curve[0:800,1]
+TTs=curve[0:1999,1]
+EEs=curve[0:1999,2]
+TEs=curve[0:1999,3]
+BBs=curve[0:1999,4]
 
-errors=np.zeros(182)
-for i in np.arange(2,184,1):
-    errors[i-2]=(TTs[i-2]+NNs[i-2]/np.sqrt((i+0.5)*f_sky*delta_l))
+errors=np.zeros(1999)
 
-print(errors)
+#对于TT EE BB
+
+for i in np.arange(2,2001,1):
+    errors[i-2]=((BBs[i-2]+NNs[i-2])/np.sqrt((i+0.5)*f_sky*delta_l))
+'''
+#对于TE
+for i in np.arange(2,2001,1):
+    errors[i-2]=np.sqrt((TEs[i-2]**2+(TTs[i-2]+NNs[i-2])*(EEs[i-2]+NNs[i-2]))/((2*i+1)*f_sky*delta_l))
+'''
+
+plt.loglog(np.arange(2,2001,1),BBs[0:1999])
+plt.errorbar(np.arange(2,2001,1),BBs[0:1999],yerr=errors)
+plt.xlabel("l")
+plt.ylabel(r"$C_l^{BB}$")
+plt.show()
--- week2_noise/noise.py
+++ week2_noise/noise.py
@@ -1,23 +1,31 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-#l：1-2500
+#l：2-800
 #CMB temparature: muK
-Tcmb=2.75*10**3
+Tcmb=2.75*10**6
 #sensitivity of each channel: muK-arcmin
-sigma_nu0=[37.42,33.46,21.31,16.87,12.07,11.30,10.34,7.69,7.25,5.57,7.05,10.79,13.80,21.95,47.45]
+#sigma_nu0=[37.42,33.46,21.31,16.87,12.07,11.30,10.34,7.69,7.25,5.57,7.05,10.79,13.80,21.95,47.45]
+sigma_nu0=[2]
 #sensitivity of each channel: muK-rad
 sigma_nu=[i*np.pi/180/60 for i in sigma_nu0]
 
 #beam size: arcmin
-theta_nu0=[70.5,58.5,51.1,41.6,36.9,33.0,30.2,26.3,23.7,28.9,28.0,24.7,22.5,20.9,17.9]
+#theta_nu0=[70.5,58.5,51.1,41.6,36.9,33.0,30.2,26.3,23.7,28.9,28.0,24.7,22.5,20.9,17.9]
+theta_nu0=[30]
 #beam size: rad
 theta_nu=[i*np.pi/180/60 for i in theta_nu0]
 
-spectrum=np.zeros(800)
-for i in np.arange(2,802,1):
+spectrum=np.zeros(1999)
+for i in np.arange(2,2001,1):
     sum=0
     for j in range(len(sigma_nu)):
-        sum+=1.0/((sigma_nu[j]/Tcmb)**2*np.exp(i*(i+1)*theta_nu[j]/8/np.log(2)))
+        sum+=1.0/((sigma_nu[j]/Tcmb)**2*np.exp(i*(i+1)*theta_nu[j]**2/8/np.log(2)))
     spectrum[i-2]=1.0/sum
 
-np.save("week2_noise/spectrum.npy",spectrum)
+np.save("spectrum.npy",spectrum)
+
+plt.plot(np.arange(2,2001,1),spectrum[0:1999])
+plt.xlabel("l")
+plt.ylabel("N")
+plt.show()

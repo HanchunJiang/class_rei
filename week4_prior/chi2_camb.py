fix normalization

week4_prior/chi2_camb.py
@@ -25,7 +25,7 @@ def error(PS,NN):
     return errors
 
 #chi2
-def chi2(Cl_fid,Cl,sigma):
+def chi2_Gauss(Cl_fid,Cl,sigma):
     '''
     Cl_fid: ground truth
     Cl: PS we want to calculate chi2
@@ -37,6 +37,14 @@ def chi2(Cl_fid,Cl,sigma):
         #print(sum)
     print(sum)
     return sum
+
+def chi2(Cl_fid,Cl,NN):
+    sum=0
+    for i in range(min(len(Cl),len(NN))):
+        sum+=(2*(i+2)+1)*((Cl_fid[i]+NN[i])/(Cl[i]+NN[i])+np.log(Cl[i]+NN[i])-(2*(i+2)-1)/(2*(i+2)+1)*np.log(Cl_fid[i]+NN[i]))
+    print(sum)
+    return sum
+
 
 #========result========#
 chi2_BB=np.zeros((len(np.arange(float(p1_value-5*p1_sigma),float(p1_value+5*p1_sigma),float(p1_sigma/10.0))),len(np.arange(float(p2_value-5*p2_sigma),float(p2_value+5*p2_sigma),float(p2_sigma/10.0)))))
@@ -62,7 +70,11 @@ for p1 in np.arange(float(p1_value-5*p1_sigma),float(p1_value+5*p1_sigma),float(
         else:
             data=np.loadtxt('output/chi1_'+str(j)+'_'+str(int(i/100))+'_'+str(i%100)+'_cl.dat')
         BBs=data[0:2000,2]
-        chi2_BB[j,i]=chi2(BB_fid,BBs,errors_BB)
+        chi2_BB[j,i]=chi2(BB_fid,BBs,spectrum)#errors_BB)
 
-np.save("chi21_BB_prior.npy",chi2_BB)
+max_chi=np.max(chi2_BB)
+for i in range(chi2_BB.shape[0]):
+    for j in range(chi2_BB.shape[1]):
+        chi2_BB[i,j]=chi2_BB[i,j]-max_chi
+np.save("chi21_BB_Wishert.npy",chi2_BB)
 

week4_prior/drawchi2.py
@@ -18,12 +18,12 @@ def posterior(chi2,tau,tau0,sigma_tau):
     print(post)
     return post
 
-chi2_BB=np.load("/home/hcjiang/class/chi21_BB_prior.npy")
+chi2_BB=np.load("/home/hcjiang/class/chi21_BB_Wishert.npy")
 
 fig = plt.figure()
 ay=fig.add_subplot()
 X,Y = np.meshgrid(np.arange(float(p2_value-5*p2_sigma),float(p2_value+5*p2_sigma),float(p2_sigma/10.0)),np.arange(float(p1_value-5*p1_sigma),float(p1_value+5*p1_sigma),float(p1_sigma/10.0)))
-contour=ay.contourf(X,Y,posterior(chi2_BB,np.arange(float(p2_value-5*p2_sigma),float(p2_value+5*p2_sigma),float(p2_sigma/10.0)),0.0561,0.0071))#,colors=['blue','lightsteelblue','white'])
+contour=ay.contourf(X,Y,posterior(chi2_BB,np.arange(float(p2_value-5*p2_sigma),float(p2_value+5*p2_sigma),float(p2_sigma/10.0)),0.0561,1e-4))#,colors=['blue','lightsteelblue','white'])
 ay.set_xlabel(p2_name)
 ay.set_ylabel(p1_name)
 fig.colorbar(contour)

week4_prior/fisher.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
 
 #======parameters=========#
 Tcmb=2.75*10**6
@@ -6,7 +8,7 @@ sigma_nu=2*np.pi/180/60
 theta_nu=30*np.pi/180/60
 f_sky=1
 delta_l=1
-sigma_tau_planck=0.0071
+sigma_tau_planck=1e-4#0.0071
 
 #=========input==============#
 data_lfid=np.loadtxt('reio_camb00_cl.dat')
@@ -17,6 +19,12 @@ data_l2m=np.loadtxt('reio_camb04_cl.dat')
 
 delta_1=0.00005
 delta_2=0.00001
+
+p1_value=0.1
+p2_value=0.0561
+
+p1_name="r"
+p2_name="tau"
 
 #========function==============#
 def Fisher(fid,NN,ip,im,jp,jm,delta_i,delta_j):
@@ -37,7 +45,59 @@ for i in np.arange(2,2002,1):
 
 F_11=1.5*Fisher(BB_lfid,spectrum,BB_l1p,BB_l1m,BB_l1p,BB_l1m,delta_1,delta_1)
 F_22=1.5*(Fisher(BB_lfid,spectrum,BB_l2p,BB_l2m,BB_l2p,BB_l2m,delta_2,delta_2)+1/sigma_tau_planck**2)
-F_12=Fisher(BB_lfid,spectrum,BB_l1p,BB_l1m,BB_l2p,BB_l2m,delta_1,delta_2)
+F_12=1.5*Fisher(BB_lfid,spectrum,BB_l1p,BB_l1m,BB_l2p,BB_l2m,delta_1,delta_2)
+
+p1_sigma=np.sqrt(1/F_11)
+p2_sigma=np.sqrt(1/F_22)
+p1p2_sigma=np.sqrt(F_11*F_22)/F_12
+
+F  = np.array([[F_11,F_12], [F_12, F_22]])  
+print(F)  
+C=np.linalg.inv(F)
+print(C)
+p1_sigma=np.sqrt(C[0,0])
+p2_sigma=np.sqrt(C[1,1])
+p1p2_sigma=C[0,1]
+
+a=np.sqrt((p1_sigma**2+p2_sigma**2)/2.0+np.sqrt((p1_sigma**2-p2_sigma**2)**2/4.0+p1p2_sigma**2))
+b=np.sqrt((p1_sigma**2+p2_sigma**2)/2.0-np.sqrt((p1_sigma**2-p2_sigma**2)**2/4.0+p1p2_sigma**2))
+theta=np.arctan(2*p1p2_sigma/(p1_sigma**2-p2_sigma**2))/2.0
+
+
+'''
+rs=np.arange(float(p1_value-5*p1_sigma),float(p1_value+5*p1_sigma),float(p1_sigma/10.0))
+taus=np.arange(float(p2_value-5*p2_sigma),float(p2_value+5*p2_sigma),float(p2_sigma/10.0))
+
+post=np.zeros((len(rs),len(taus)))
+for i in range(len(rs)):
+    for j in range(len(taus)):
+        post[i,j]=np.exp(-0.5*rs[i]*F_12*taus[j])
+
+fig = plt.figure()
+ay=fig.add_subplot()
+X,Y = np.meshgrid(np.arange(float(p2_value-5*p2_sigma),float(p2_value+5*p2_sigma),float(p2_sigma/10.0)),np.arange(float(p1_value-5*p1_sigma),float(p1_value+5*p1_sigma),float(p1_sigma/10.0)))
+contour=ay.contourf(X,Y,post)#,colors=['blue','lightsteelblue','white'])
+ay.set_xlabel(p2_name)
+ay.set_ylabel(p1_name)
+fig.colorbar(contour)
+plt.savefig("fisher.jpg")
+plt.show()
+'''
+
+fig = plt.figure(0)
+ax = fig.add_subplot(111, aspect='equal')
+e = Ellipse(xy = (p1_value,p2_value), width = a * 2, height = b * 2, angle=theta*180/3.14)
+ax.add_artist(e)
+ax.set_xlabel(p1_name)
+ax.set_ylabel(p2_name)
+plt.xlim(p1_value-2*a, p1_value+2*a)
+plt.ylim(p2_value-5*b,p2_value+5*b)
+plt.savefig("fisher.jpg")
+plt.show()
+
+print(p1_sigma)
+print(p2_sigma)
+print(p1p2_sigma)
 
 print(np.sqrt(1/F_11))
 print(np.sqrt(1/F_22))

week4_prior/sigma.py
@@ -10,7 +10,7 @@ p2_value=0.0561
 p2_sigma=0.0001085
 p2_name="tau_reio"#input("p2_name ")
 
-chi2_BB=np.load("/home/hcjiang/class/chi21_BB_prior.npy")
+chi2_BB=np.load("/home/hcjiang/class/chi21_BB_Wishert.npy")
 
 #=====function==========#
 def posterior(chi2,tau,tau0,sigma_tau):
@@ -22,8 +22,10 @@ def posterior(chi2,tau,tau0,sigma_tau):
     print(post)
     return post
 
-def func(r,sigma,mu):
-    return np.exp(-(r-mu)**2/2/sigma**2)
+def func(r,p,mu):
+    sigma=p[0]
+    N=p[1]
+    return N*np.exp(-(r-mu)**2/2/sigma**2)
 
 def residuals(p,y,x,mu):
     return y-func(x,p,mu)
@@ -34,16 +36,25 @@ post=posterior(chi2_BB,np.arange(float(p2_value-5*p2_sigma),float(p2_value+5*p2_
 post_p1=np.zeros(post.shape[0])
 post_p2=np.zeros(post.shape[1])
 
+#sum=0
 for i in range(post.shape[0]):
     for j in range(post.shape[1]):
-        post_p1[i]+=post[i,j]*p1_sigma/10
+        post_p1[i]+=post[i,j]*p2_sigma/10
+        #sum+=post_p1[i]*p1_sigma/10
 
+#post_p1=[post_p1[i]/sum for i in range(len(post_p1))]
+#print(sum)
+#sum=0
 for i in range(post.shape[1]):
     for j in range(post.shape[0]):
-        post_p2[i]+=post[j,i]*p2_sigma/10
+        post_p2[i]+=post[j,i]*p1_sigma/10
+        #sum+=post_p2[i]*p2_sigma/10
 
+#post_p2=[post_p2[i]/sum for i in range(len(post_p2))]
+#print(sum)
 sigma0=1e-4
-plsq=leastsq(residuals,sigma0,args=(post_p1,np.arange(float(p1_value-5*p1_sigma),float(p1_value+5*p1_sigma),float(p1_sigma/10.0)),0.1))
+N0=1
+plsq=leastsq(residuals,[sigma0,N0],args=(post_p1,np.arange(float(p1_value-5*p1_sigma),float(p1_value+5*p1_sigma),float(p1_sigma/10.0)),0.1))
 print(plsq[0])
 plt.plot(np.arange(float(p1_value-5*p1_sigma),float(p1_value+5*p1_sigma),float(p1_sigma/10.0)),post_p1)
 plt.plot(np.arange(float(p1_value-5*p1_sigma),float(p1_value+5*p1_sigma),float(p1_sigma/10.0)),[func(i,plsq[0],0.1) for i in np.arange(float(p1_value-5*p1_sigma),float(p1_value+5*p1_sigma),float(p1_sigma/10.0))])
@@ -54,9 +65,11 @@ plt.show()
 plt.cla()
 
 sigma0=1e-4
-plsq=leastsq(residuals,sigma0,args=(post_p2,np.arange(float(p2_value-5*p2_sigma),float(p2_value+5*p2_sigma),float(p2_sigma/10.0)),0.0561))
+N0=1
+plsq=leastsq(residuals,[sigma0,N0],args=(post_p2,np.arange(float(p2_value-5*p2_sigma),float(p2_value+5*p2_sigma),float(p2_sigma/10.0)),0.0561))
 print(plsq[0])
 plt.plot(np.arange(float(p2_value-5*p2_sigma),float(p2_value+5*p2_sigma),float(p2_sigma/10.0)),post_p2)
+plt.plot(np.arange(float(p2_value-5*p2_sigma),float(p2_value+5*p2_sigma),float(p2_sigma/10.0)),[func(i,plsq[0],0.0561) for i in np.arange(float(p2_value-5*p2_sigma),float(p2_value+5*p2_sigma),float(p2_sigma/10.0))])
 plt.xlabel(p2_name)
 plt.ylabel("P("+p2_name+")")
 plt.savefig(p2_name+"2D_posterior.jpg")